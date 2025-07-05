673ab7c36f792138726e9f79dce6822f50cec4bd
chevoisiatesalvati <andrea_salvemini@yahoo.it>
2025-07-05 04:33:37 +0200

    delete .next folder

--- .next/static/chunks/src_app_page_tsx_019caab6._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/8df1b_@noble_curves_esm_secp256k1_030befa2.js",
-  "static/chunks/node_modules_7e4c9fab._.js",
-  "static/chunks/_a7b3179f._.js"
-],
-    source: "dynamic"
-});
